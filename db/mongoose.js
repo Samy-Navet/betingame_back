92f5d6f2556ff9d88c9dc7738521ee54af5cb81e
Navet <samynavet@gmail.com>
2019-01-21 21:15:03 +0100

    mise en prod heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/betingame');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/betingame');
 
 module.exports = {mongoose};
