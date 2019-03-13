95f8d0fa50851394953a086fea3ce82f92766656
Navet <samynavet@gmail.com>
2019-03-13 15:31:12 +0100

    liste des paris

--- controllers/bet/betsList.js
+++ controllers/bet/betsList.js
@@ -1,29 +1,36 @@
 var {Bet} = require('./../../Models/Bet');
 var {Match} = require('./../../Models/Match');
-const _ = require('lodash')
+const _ = require('lodash');
 
 var betsList = (req,res) =>{
     var body = req.body;
     var id = req.params.id
-    Bet.find({userid : id}).then((bets) =>{
+    Bet.find({userid : id}).select('-__v -matchs._id').lean().then((bets) =>{
         var betsList = bets;
-        for(var j = 0; j < betsList.length; j++){
-            var matchs = betsList[j].matchs
-            promises = [];
-            for(var i = 0; i < matchs.length; i++){
-                promises.push(new Promise((resolve,reject) =>{
-                    Match.findOne({_id : matchs[i].matchid}).then((matchDetails) =>{
-                        console.log(j);
-                        betsList[j].matchdetails = matchDetails;
-                        resolve();
-                    });
-                }))
+        var matchQuery = [];
+        for(var i = 0; i < betsList.length; i++){
+            var matchs = betsList[i].matchs
+            for(j = 0; j < matchs.length; j++){
+                matchQuery.push(betsList[i].matchs[j].matchid);
             }
-            Promise.all(promises);
         }
-        res.send(bets);
-    }).catch((e) => {
-        res.send(e);
+    
+        Match.find({_id : {$in : matchQuery}}).select('-participant.coteparticipant -__v').then((matchDetails) =>{
+            betsList.forEach((bet, betindex) => {
+                bet.matchs.forEach((matchBet, matchBetIndex)=>{
+                    matchDetails.forEach((match, matchindex) =>{
+                        if(matchBet.matchid == match._id){
+                            var matchdata = _.pick(match, ['dates','game','title','participant'])
+                            betsList[betindex].matchs[matchBetIndex] = {...betsList[betindex].matchs[matchBetIndex], ...matchdata};
+                        }
+                    })
+                })
+            })
+
+            res.send(betsList);
+        });
+    }).catch((betsErr) => {
+        res.send(betsErr);
     })
 }
 
