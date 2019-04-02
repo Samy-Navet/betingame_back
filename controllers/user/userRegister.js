14f8fceab4e37ba83d58f78edaed6b6e3a104cc3
Navet <samynavet@gmail.com>
2019-04-02 12:27:24 +0200

    jsdoc user

--- controllers/user/userRegister.js
+++ controllers/user/userRegister.js
@@ -1,3 +1,35 @@
+/**
+ * @api {post} /user/ Register and request user information
+ * @apiName userRegister
+ * @apiGroup User
+ *
+ * @apiParam {String} username User username string.
+ * @apiParam {String} password User password string.
+ * @apiParam {String} email User email string.
+ * @apiParam {Boolean} admin Users state boolean.
+ *
+ * @apiSuccess {String} _id id of the User.
+ * @apiSuccess {String} username username of the User.
+ * @apiSuccess {String} email email of the User.
+ * @apiSuccess {String} admin state of the User.
+ * @apiSuccess {Object[]} tokens array of tokens of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *           "_id": "5ca3169662ca77093cc91f22",
+ *           "username": "toto",
+ *           "email": "toto@gmail.com",
+ *           "admin": false,
+ *           "tokens": [
+ *               {
+ *                   "access": "auth",
+ *                   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2EzMTY5NjYyY2E3NzA5M2NjOTFmMjIiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTU0MTkyMDIyfQ.6pdnH28nqxj4jVVF90kwK41RQfuiPCMMm_j08BexmkA"
+ *               }
+ *           ]
+ *       }
+ *
+ */
 var {User} = require('./../../Models/User');
 
 const userRegister = (req, res) =>{
@@ -20,7 +52,7 @@ const userRegister = (req, res) =>{
     }).then((data) =>{
         res.header('x-auth', data.token).send(user);
     }).catch((e) => {
-        res.status(400).send(e);
+        res.status(500).send(e);
     })
 };
 
