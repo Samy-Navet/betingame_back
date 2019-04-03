e8b184e1d57e4861c6c53de98e3db3728f89f7bc
Navet <samynavet@gmail.com>
2019-04-03 16:01:47 +0200

    test travis CI mocha

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/betingame',{ useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/betingame_test' || 'mongodb://localhost:27017/betingame_test',{ useNewUrlParser: true, useCreateIndex: true }).then(()=>{
+    console.log('connected to database');
+}).catch((err)=>{
+    console.log(err);
+})
 
 module.exports = {mongoose};
---
+++ test/test1.js
@@ -0,0 +1,71 @@
+var {mongoose} = require('./../db/mongoose');
+var {User} = require('./../Models/User');
+
+
+// beforeEach(function() {
+//     return User.remove({}).then(function() {
+//         var username = 'tata';
+//         var password = 'tata123';
+//         var email = 'tata@gmail.com';
+
+//         var user = new User({
+//             username: username,
+//             email: email,
+//             password: password,
+//             money: 500
+//         });
+
+//         if(typeof admin !== 'undefined' && admin != null){
+//             user.admin = admin
+//         }
+
+//         user.save().then(() =>{
+//             user.generateAuthToken()
+//         }).then((data) =>{
+//             return data;
+//             // res.header('x-auth', data.token).send(user);
+//         }).catch((e) => {
+//             return e;
+//             console.log(e);
+//         })
+//     });
+// });
+
+//   describe('register an user', function() {
+//     it('should save a user and return it', function() {
+
+//          User.find({}, function(err, res){
+//             if(err) return done(err);
+//             res.should.have.length(1);
+//             done();
+//          });
+//     });
+//   });
+
+beforeEach(function() {
+    return User.remove({});
+  });
+
+  describe('register an user', function() {
+    it('should save a user and return it', function(done) {
+        var username = 'tata';
+        var password = 'tata123';
+        var email = 'tata@gmail.com';
+
+        var user = new User({
+            username: username,
+            email: email,
+            password: password,
+            money: 500
+        });
+        if(typeof admin !== 'undefined' && admin != null){
+            user.admin = admin
+        }
+
+        user.save(function(err) {
+            if (err) done(err);
+            else done();
+        });
+    });
+});
+
