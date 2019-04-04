719032951f8f4d0ccd2244313ed5d0ae9d638830
Navet <samynavet@gmail.com>
2019-04-04 16:28:54 +0200

    allow cors

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/doc', express.static(__dirname + '/doc/'));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 userRoute(app);
 matchRoute(app);
