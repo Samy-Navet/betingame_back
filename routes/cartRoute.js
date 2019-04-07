b1b796c8834270cc2fef798fcbe3d2e12c308439
Navet <samynavet@gmail.com>
2019-04-07 21:42:46 +0200

    get cart details

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,6 +1,7 @@
 const cartCreate = require('./../controllers/cart/cartCreate');
 const cartDeleteAll = require('./../controllers/cart/cartDeleteAll');
 const cartDeleteMatch = require('./../controllers/cart/cartDeleteMatch');
+const cartDetails = require('./../controllers/cart/cartDetails');
 
 
 
@@ -8,6 +9,7 @@ module.exports = (app) =>{
 app.route('/user/:id/cart')
     .post(cartCreate)
     .delete(cartDeleteAll)
+    .get(cartDetails)
 
 app.route('/user/:id/cart/:match')
     .delete(cartDeleteMatch)
