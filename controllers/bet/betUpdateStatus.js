f07b0b3749db202e7469d6a2215ef46b659a6e1c
Navet <samynavet@gmail.com>
2019-04-23 15:48:16 +0200

    update des paris apres mise à jour d'un match

--- controllers/bet/betUpdateStatus.js
+++ controllers/bet/betUpdateStatus.js
@@ -61,4 +61,64 @@ const betUpdateStatus = (req,res) => {
     })
 }
 
-module.exports = betUpdateStatus;
+const updateBetsAfterMatch = (id_match) => {
+    Bet.find({'matchs.matchid': id_match}).lean().then((bets) =>{
+        bets.forEach((bet, betIndex) =>{
+            var betid = bets[betIndex]._id;
+            var userid = bets[betIndex].userid;
+            var matchs = bets[betIndex].matchs;
+            matchQuery = [];
+            for(var i = 0; i < matchs.length; i++){
+                matchQuery.push(matchs[i].matchid);
+            }
+            // apres la maj du modele match
+            Match.find({_id : {$in : matchQuery}}).lean().then((matchsDetails) =>{
+                var matchNumber = 0;
+                var endedMatchNumber = 0;
+                var matchSuccess = 0;
+                matchsDetails.forEach((match, matchindex)=>{
+                    var betMatch = jp.query(bets[betIndex],'$.matchs[?(@.matchid=="'+match._id+'")]');
+                    if(match.state == 2 && match.winner && match.winner !== null){
+                        endedMatchNumber++;
+                        if(match.winner == betMatch[0].participantchoice){
+                            matchSuccess++;        
+                        }
+                    }
+                    matchNumber++;
+                });
+                if(endedMatchNumber > 0){
+                    if(endedMatchNumber === matchNumber && matchSuccess === matchNumber){
+                        // update status ended + give credit
+                        // modifier user model before this !!
+                        Bet.updateOne({_id: betid}, {$set : {status: 2}}, {new: true}).then((result)=>{
+                            User.findOneAndUpdate({_id: userid},{$inc :{money : (bets[betIndex].cotetotale * bets[betIndex].bet), score : (bets[betIndex].cotetotale * bets[betIndex].bet)}},{new : true}).then((user)=>{
+                                // res.status(200).send(result);
+                                console.log('bet success')
+                            })
+                        })
+                    }
+                    else if(endedMatchNumber === matchNumber){
+                        // update status ended
+                        Bet.updateOne({_id: betid}, {$set : {status: -1}}, {new: true}).then((result)=>{
+                            // res.status(200).send(result);
+                            console.log('bet lost')
+                        })
+                    }
+                    else
+                    {
+                        // update status in progress
+                        Bet.updateOne({_id: betid}, {$set : {status: 1}}, {new : true}).then((result)=>{
+                            // res.status(200).send(result);
+                            console.log('bet in progress')
+                        })
+                    }
+                }
+            }).catch((matcherr) =>{
+                console.log(matcherr);
+            })          
+        })
+    })
+
+}
+
+module.exports = {betUpdateStatus, updateBetsAfterMatch};
